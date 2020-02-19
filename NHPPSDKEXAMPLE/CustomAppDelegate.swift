31e88866007100557e9836dc834806ea102bda1e
Luis Gutiérrez <luis.gutierrez@pfcti.com>
2020-02-19 10:26:09 -0600

    ResetLastCommit

--- NHPPSDKEXAMPLE/CustomAppDelegate.swift
+++ NHPPSDKEXAMPLE/CustomAppDelegate.swift
@@ -12,8 +12,6 @@ import UIKit
 class CustomAppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         
-        
-        
         return true
     }
 }
