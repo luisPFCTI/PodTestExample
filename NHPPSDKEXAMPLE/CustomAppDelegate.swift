a281b041a480495e945357b1cff60be23e6ffc3b
Luis Gutiérrez <luis.gutierrez@pfcti.com>
2020-02-20 12:16:31 -0600

    update CustomAppDelegate

--- NHPPSDKEXAMPLE/CustomAppDelegate.swift
+++ NHPPSDKEXAMPLE/CustomAppDelegate.swift
@@ -11,7 +11,11 @@ import UIKit
 
 open class CustomAppDelegate: UIResponder, UIApplicationDelegate {
     open func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        
+        print(url.absoluteURL)
+        print(url.scheme!)
+        print(url.host!)
+        print(url.path)
+        print(url.pathComponents)
         return true
     }
 }
