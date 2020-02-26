f8ea5f096b535c3225496df76daffa7098bbc56c
Luis Gutiérrez <luis.gutierrez@pfcti.com>
2020-02-26 15:19:35 -0600

    new class connection

--- NHPPSDKEXAMPLE/Manager.swift
+++ NHPPSDKEXAMPLE/Manager.swift
@@ -1,5 +1,4 @@
 import Foundation
-import Alamofire
 
 public class Response {
     var requestID: String
@@ -13,56 +12,57 @@ public class Response {
 
 class NHPPConnectionManager {
     public func activateToken(customerId: String) -> Bool {
+        return getActivationToken(customerId: customerId)
+    }
+    
+    private func getActivationToken(customerId: String) -> Bool {
+        var isOK = false
             self.activateToken(customerId: customerId, onSuccess: { response in
                 if response?.returnCode == "00" {
+                    isOK = true
                 }
             }, onError: { error in
                 if error?.returnCode != "00" {
+                    isOK = false
                 }
             })
-        return true
+        return isOK
     }
     
     private func activateToken(customerId: String, onSuccess
     successBlock: ((Response?) -> Void)!,
                        onError errorBlock: ((Response?) -> Void)!) {
-        let headers: HTTPHeaders = [.contentType("application/json"),
-                                    .init(name: "Ocp-Apim-Subscription-Key", value: "584d7d57364049548c2381c7aa651f7d"),
-                                    .init(name: "Ocp-Apim-Trace", value: "true")
-                                    ]
-        let parameters = [
-            "requestID": "4",
-            "institutionCode": "HST",
-            "tokenRefID": "DNITHE381502386342002358",
-            "activationCode": "0153632",
-            "operatorID": "14",
-            "reason": "Token activation pending",
-            "clientId": customerId
-        ]
-        var requestId: String?
-        var returnCode: String?
-        let semaphore = DispatchSemaphore(value: 0)
-        let ok = AF.request("https://proxybilletera-cert.pfcti.com/activetoken",method: .post, parameters: parameters, headers: headers).response(completionHandler: { response in
-            debugPrint(response)
-            print("holalucho")
-            debugPrint("holalucho")
-            switch response.result {
-            case .success(let data):
-                let dictionary = data as! NSDictionary
-                requestId = dictionary.value(forKey: "requestID") as? String
-                returnCode = dictionary.value(forKey: "returnCode") as? String
-                let response = Response(requestID: requestId ?? "", returnCode: returnCode ?? "")
-                successBlock(response)
-                semaphore.signal()
-            case .failure(let error):
-                requestId = "403"
-                returnCode = error.localizedDescription
-                let response = Response(requestID: requestId ?? "", returnCode: returnCode ?? "")
-                errorBlock(response)
-                semaphore.signal()
+        let url = URL(string: "https://proxybilletera-cert.pfcti.com/activetoken")!
+        var request = URLRequest(url: url)
+        request.setValue("584d7d57364049548c2381c7aa651f7d", forHTTPHeaderField: "Ocp-Apim-Subscription-Key")
+        request.setValue("true", forHTTPHeaderField: "Ocp-Apim-Trace")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                fatalError("Error: \(error.localizedDescription)")
+                successBlock(Response(requestID: "requestId" ?? "", returnCode: "99" ?? ""))
             }
-        })
-        _ = semaphore.wait(wallTimeout: .distantFuture)
-        print(ok)
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                fatalError("Error: invalid HTTP response code")
+                successBlock(Response(requestID: "requestId" ?? "", returnCode: "00" ?? ""))
+            }
+            guard let data = data else {
+                fatalError("Error: missing response data")
+                successBlock(Response(requestID: "requestId" ?? "", returnCode: "00" ?? ""))
+            }
+
+            do {
+//                let decoder = JSONDecoder()
+//                let posts = try decoder.decode([Post].self, from: data)
+//                print(posts.map { $0.title })
+                print(data)
+                successBlock(Response(requestID: "requestId" ?? "", returnCode: "00" ?? ""))
+            }
+            catch {
+                print("Error: \(error.localizedDescription)")
+                successBlock(Response(requestID: "requestId" ?? "", returnCode: "99" ?? ""))
+            }
+        }
+        task.resume()
     }
 }
